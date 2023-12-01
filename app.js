81faf2e2f13dd9ec0b2b669d18a19c0c72251565
Sabnit <sabnit111@gmail.com>
2023-12-01 07:00:04 +0545

    made some changes in css

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// Default values for transition and hold times
-const DEFAULT_TRANSITION = 500;
-const DEFAULT_HOLD = 1000;
+const DEFAULT_TRANSITION = 400;
+const DEFAULT_HOLD = 2000;
+const DEFAULT_IMAGE_WIDTH = 0;
 
-// Carousel class to manage the image carousel
 class Carousel {
   constructor(container, transition = DEFAULT_TRANSITION, hold = DEFAULT_HOLD) {
-    // Store carousel elements and settings
     this.container = container;
     this.slides = Array.from(container.querySelectorAll(".carousel-img"));
     this.totalSlides = this.slides.length;
     this.currentIndex = 0;
     this.transition = transition;
     this.hold = hold;
+    this.imageWidth = DEFAULT_IMAGE_WIDTH;
     this.dotsContainer = document.querySelector(".carousel-controller");
 
     this.init();
@@ -20,41 +19,39 @@ class Carousel {
   }
 
   init() {
-    this.setupSlideTransition();
-    this.startAutomaticSlide();
+    this.designCarousel();
+    this.startCarousel();
   }
 
-  setupSlideTransition() {
+  designCarousel() {
     this.slides.forEach((slide) => {
       slide.style.transition = `transform ${this.transition}ms`;
     });
-
-    this.container.style.width = `${
-      this.totalSlides * this.slides[0].offsetWidth
-    }px`;
+    this.imageWidth = this.slides[0].offsetWidth;
+    this.container.style.width = `${this.totalSlides * this.imageWidth}px`;
   }
 
-  startAutomaticSlide() {
+  startCarousel() {
     setInterval(() => {
-      this.moveToNextImage();
+      this.moveNext();
     }, this.hold);
   }
 
-  moveToNextImage() {
+  moveNext() {
     this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
-    this.slideTransition();
-    this.updateNavigationDots();
+    this.transitionSlide();
+    this.updateDots();
   }
 
-  moveToPreviousImage() {
+  movePrevious() {
     this.currentIndex =
       (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
-    this.slideTransition();
-    this.updateNavigationDots();
+    this.transitionSlide();
+    this.updateDots();
   }
 
-  slideTransition() {
-    const offset = -1 * this.currentIndex * this.slides[0].offsetWidth;
+  transitionSlide() {
+    const offset = -1 * this.currentIndex * this.imageWidth;
     this.container.style.transform = `translateX(${offset}px)`;
   }
 
@@ -70,11 +67,10 @@ class Carousel {
       });
       dotController.appendChild(dot);
     }
-    this.updateNavigationDots();
+    this.updateDots();
   }
 
-  // Update navigation dots to highlight the current slide
-  updateNavigationDots() {
+  updateDots() {
     const dots = this.dotsContainer.querySelectorAll(".dot");
     dots.forEach((dot, index) => {
       if (index === this.currentIndex) {
@@ -85,28 +81,26 @@ class Carousel {
     });
   }
 
-  // Handle click events for left and right arrows
   controlHandler() {
     const leftArrow = document.querySelector(".left-arrow");
     const rightArrow = document.querySelector(".right-arrow");
 
     leftArrow.addEventListener("click", () => {
-      this.moveToPreviousImage();
+      this.movePrevious();
     });
 
     rightArrow.addEventListener("click", () => {
-      this.moveToNextImage();
+      this.moveNext();
     });
   }
 
   goToSlide(index) {
     this.currentIndex = index;
-    this.slideTransition();
-    this.updateNavigationDots();
+    this.transitionSlide();
+    this.updateDots();
   }
 }
 
-// Initializes the carousel after the DOM is loaded
 document.addEventListener("DOMContentLoaded", function () {
   const carouselContainer = document.querySelector(".carousel-wrapper");
   const carousel = new Carousel(carouselContainer);
