082a5b695aac4af441261414fa617177dd40b803
Sabnit <sabnit111@gmail.com>
2023-12-01 21:50:27 +0545

    made some changes readme.md

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-## Hosted on
+# Carousel App
 
-https://sabnit.github.io/Image-Carousel/
+A simple carousel application that enables smooth transitions between images in a slideshow format.
+
+## Features
+
+Automatic Slideshow: Enables automatic transitioning between slides at defined intervals.
+Manual Navigation: Allows users to move to the next or previous slide manually.
+Dot Indicators: Provides visual cues for the current active slide through dots at the bottom of the carousel.
+
+## File Structure
+
+app.js: Contains the Carousel class and logic for the carousel functionality.
+constants.js: Defines default constants for transition durations and other configurations.
+index.html: Sample HTML structure to demonstrate how to integrate the carousel into a web page.
+
+## Configuration
+
+**Transition Duration (DEFAULT_TRANSITION):** Duration (in milliseconds) for slide transitions.
+**Hold Duration (DEFAULT_HOLD):** Time interval (in milliseconds) between slide transitions.
+**Image Width (DEFAULT_IMAGE_WIDTH):** Default width for each slide image.
--- src/app.js
+++ src/app.js
@@ -1,4 +1,10 @@
 class Carousel {
+  /**
+   * @param {HTMLElement} container - The main container element for the carousel.
+   * @param {number} [transition=DEFAULT_TRANSITION] - Transition duration between slides (in milliseconds).
+   * @param {number} [hold=DEFAULT_HOLD] - Time interval between slide transitions (in milliseconds).
+   */
+
   constructor(container, transition = DEFAULT_TRANSITION, hold = DEFAULT_HOLD) {
     this.container = container;
     this.slides = Array.from(container.querySelectorAll(".carousel-img"));
@@ -9,36 +15,55 @@ class Carousel {
     this.imageWidth = DEFAULT_IMAGE_WIDTH;
     this.dotsContainer = document.querySelector(".carousel-controller");
 
+    // Initialize the carousel
     this.init();
     this.setupDots();
     this.controlHandler();
   }
 
+  /**
+   * Initializes the carousel.
+   */
   init() {
     this.designCarousel();
     this.startCarousel();
   }
 
+  /**
+   * Sets up the initial carousel layout and transitions.
+   */
   designCarousel() {
+    // Apply transition to slides
     this.slides.forEach((slide) => {
       slide.style.transition = `transform ${this.transition}ms`;
     });
+
+    // Set container width to hold all slides
     this.imageWidth = this.slides[0].offsetWidth;
     this.container.style.width = `${this.totalSlides * this.imageWidth}px`;
   }
 
+  /**
+   * Starts the carousel automatic sliding.
+   */
   startCarousel() {
     setInterval(() => {
       this.moveNext();
     }, this.hold);
   }
 
+  /**
+   * Moves to the next slide in the carousel.
+   */
   moveNext() {
     this.currentIndex = (this.currentIndex + 1) % this.totalSlides;
     this.transitionSlide();
     this.updateDots();
   }
 
+  /**
+   * Moves to the previous slide in the carousel.
+   */
   movePrevious() {
     this.currentIndex =
       (this.currentIndex - 1 + this.totalSlides) % this.totalSlides;
@@ -46,11 +71,17 @@ class Carousel {
     this.updateDots();
   }
 
+  /**
+   * Applies transition to slide according to current index.
+   */
   transitionSlide() {
     const offset = -1 * this.currentIndex * this.imageWidth;
     this.container.style.transform = `translateX(${offset}px)`;
   }
 
+  /**
+   * Sets up the dot navigation for slides.
+   */
   setupDots() {
     const dotController = document.querySelector(".dot-controller");
 
@@ -66,6 +97,9 @@ class Carousel {
     this.updateDots();
   }
 
+  /**
+   * Updates the active dot corresponding to the current slide.
+   */
   updateDots() {
     const dots = this.dotsContainer.querySelectorAll(".dot");
     dots.forEach((dot, index) => {
@@ -77,19 +111,28 @@ class Carousel {
     });
   }
 
+  /**
+   * Handles click events for left and right arrows.
+   */
   controlHandler() {
-    const leftArrow = document.querySelector(".left-arrow");
-    const rightArrow = document.querySelector(".right-arrow");
+    const controller = {
+      leftArrow: document.querySelector(".left-arrow"),
+      rightArrow: document.querySelector(".right-arrow"),
+    };
 
-    leftArrow.addEventListener("click", () => {
+    controller.leftArrow.addEventListener("click", () => {
       this.movePrevious();
     });
 
-    rightArrow.addEventListener("click", () => {
+    controller.rightArrow.addEventListener("click", () => {
       this.moveNext();
     });
   }
 
+  /**
+   * Navigates directly to a specific slide by its index.
+   * @param {number} index - The index of the slide to navigate to.
+   */
   goToSlide(index) {
     this.currentIndex = index;
     this.transitionSlide();
@@ -97,6 +140,7 @@ class Carousel {
   }
 }
 
+// Initialize the carousel when the DOM content is loaded
 document.addEventListener("DOMContentLoaded", function () {
   const carouselContainer = document.querySelector(".carousel-wrapper");
   const carousel = new Carousel(carouselContainer);
